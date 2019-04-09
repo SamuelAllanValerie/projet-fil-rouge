a8bb42fea5d92f8efe8456caabd15312196c54c8
Samuel Allan Valerie <17672@csmrouen.net>
2019-04-09 16:58:00 +0200

    CV CV SAM

---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!-- OPTIONAL: description -->
+    <meta name="description" content="">
+    <!-- OPTIONAL: author -->
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="icon.png" />
+   <title>Site de Samuel Val&eacute;rie</title>
+   <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- Link the main.css stylesheet -->
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- OPTIONAL: Google Fonts (don't forget to edit main.css also) -->
+    <link href="https://fonts.googleapis.com/css?family=Heebo|Oswald" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+ </head>
+ <body>
+   <div class="header" id="headerwrap">
+     <div class="container">
+       <div class="row centered">
+         <div class="col-lg-12">
+           <h1>Samuel Val&eacute;rie</h1>
+           <h2>R&eacute;f&eacute;renceur SEO
+            <a href="samuel.valerie7@gmail.com">samuel.valerie7@gmail.com</a>
+           </h2>
+         </div><!--/.col-lg-12-->
+       </div><!--/.row-->
+     </div><!--/.container-->
+   </div><!--/.#headerwrap-->
+
+   <section id="about" name="about"></section>
+ <div class="about">
+   <div class="container">
+     <div class="row">
+       <div class="col-lg-2 col-lg-offset-1">
+         <h3>&Agrave; propos</h3>
+       </div>
+       <div class="col-lg-6">
+<p>&Eacute;tudiant en recherche de stage en SEO/SEM ou encore community management &agrave; partir du 27 mai 2019. Je r&eacute;side &agrave; &Eacute;vreux et je suis actuellement en formation &agrave; la Nfactory School de Rouen, en tant que r&eacute;f&eacute;rent digital</p>
+</div>
+       <div class="col-lg-3">
+         <p><a href="samuel-valerie-cv.pdf" target="_blank"<b>Voir le PDF</b></a></p>
+       </div>
+
+     </div><!--/.row-->
+   </div><!--/.container-->
+ </div><!--/ #intro-->
+
+ <section id="resume" name="resume"></section>
+	<!-- EDUCATION SECTION -->
+	<div class="container desc">
+		<div class="row">
+
+				<div class="col-lg-2 col-lg-offset-1">
+					<h5>FORMATION</h5>
+				</div>
+				<!-- EDIT: education line 1 -->
+				<div class="col-lg-3">
+					<p><b>School Certificate Cambridge</b><br/>
+						New Eton College<br>
+            1998 &agrave; 2002
+						<!--<br/><i>Extra info here</i>-->
+					</p>
+				</div>
+				<div class="col-lg-3">
+					<p><b>NTC 3 Air Conditioning and Refrigeration</b><br/>
+					Industrial and Vocational Training Board<br/>
+                   2003 &agrave; 2004
+					</p>
+				</div>
+
+				<!-- EDIT: education line 2 -->
+				<div class="col-lg-3">
+					<p><b>Basic and Advanced Electronics </b><br/>
+						    School of Electronics<br/>
+                2005
+						<!--<br/><i>Extra info here</i>-->
+					</p>
+				</div>
+
+        <div class="col-lg-3">
+					<p><b>R&eacute;f&eacute;rent Digital</b><br/>
+					NFactory School<br/>
+                   2019 (Janvier jusqu'&agrave; pr&eacute;sent)
+					</p>
+				</div>
+
+
+		</div><!--/.row -->
+		<br>
+		<hr>
+	</div><!--/.container -->
+
+	<!-- WORK SECTION -->
+	<div class="container desc">
+		<div class="row">
+
+				<div class="col-lg-2 col-lg-offset-1">
+					<h5>EXP&Eacute;RIENCES</h5>
+				</div>
+				<!-- EDIT: work line 1 -->
+				<div class="col-lg-3">
+					<p class="tight"><b>R&eacute;f&eacute;renceur SEO</b><br/>
+						SEDECO Ltd (&Icirc;le Maurice)<br/>
+					</p>
+					<p><more>Sp&eacute;cialis&eacute; dans le Netlinking et la R&eacute;daction Web, j'ai jongl&eacute; entre le d&eacute;ploiement de strat&eacute;gies SEO et le pilotage de projets pour diff&eacute;rents types de clients de Avril 2011 &agrave; Mars 2016.</more></p>
+				</div>
+
+				<!-- EDIT: work line 2-->
+				<div class="col-lg-3">
+					<p><b>Mod&eacute;rateur et superviseur</b><br/>
+						MMS Ltd. <br/>
+					</p>
+					<p><more>Le but de mon poste consistait &agrave mod&eacute;rer divers forums priv&eacute;s et promouvoir l'interaction entre les internautes. Par la suite, j'ai &eacute;t&eacute; promu superviseur et je devais m'assurer que mon &eacute;quipe soit toujours productive de Mars 2006 à Mars 2011.</more></p>
+				</div>
+
+		</div><!--/.row -->
+
+		<hr>
+	</div><!--/.container -->
+
+
+	<!-- SKILLS SECTION -->
+	<div class="container desc">
+		<div class="row">
+				<div class="col-lg-2 col-lg-offset-1">
+					<h5>COMP&Eacute;TENCES</h5>
+				</div>
+
+				<!-- EDIT: skills line 1-->
+				<div class="col-lg-3 col-lg-offset-0">
+					   <p>Analyser le trafic d'un site Web et am&eacute;liorer son positionnement<br/></p>
+						 <p>Instaurer des solutions de tracking et analyser le comportement des internautes<br/>
+             <p>G&eacute;n&eacute;rer et maintenir l'acquisition de trafic<br/>
+             <p>Concevoir l'arborescence d'un site Web<br/>
+             <p>Administrer et g&eacute;r&eacute;r un site sous WordPress (CMS)<br/>
+             <p>Mettre en place des campagnes de publicit&eacute;s en ligne (Google et Facebook Ads)<br/>
+             <p>R&eacute;diger des textes optimis&eacute; pour les moteurs de recherche (R&eacute;daction Web)<br/>
+             <p>Animer une communaut&eacute; Web et alimenter des r&eacute;seaux sociaux<br/>
+             <p>Coordoner l'activit&eacute; d'une &eacute et piloter un projet<br/>
+             <p>Communiquer et prospecter via des outils en ligne<br/>
+             <p>Ma&icirc;triser les principaux outils de r&eacute;f&eacute;rence dont peuvent s'&eacute;quiper les petites et moyennes structures (Google Apps, Framasoft, Highrise, Salesforce, Basecamp, Trello, etc...)
+					</p>
+				</div>
+			</div><!--/.row -->
+		<br>
+	</div><!--/.container -->
+
+  <section id="resume" name="resume"></section>
+	<!-- EDUCATION SECTION -->
+	<div class="container desc">
+		<div class="row">
+
+		</div><!--/.row -->
+		<br>
+		<hr>
+	</div><!--/.container -->
+
+
+  </body>
+  <!-- WORK SECTION -->
+  <div class="container desc">
+    <div class="row">
+        <div class="col-lg-2 col-lg-offset-0">
+
+       </div>
+  <section id="contact" name="contact"></section>
+  <!--FOOTER DESCRIPTION -->
+  <div id="footwrap" class="footer">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-lg-3 col-lg-offset-1">
+          <p><b>LOISIRS</b></p>
+          <p>Billard<br/>
+            Musique<br/>
+            Jeux vid&eacute;o<br/>
+            Films<br/>
+            Cuisine Cr&eacute;ole<br/>
+          </p>
+        </div>
+        <!-- EDIT: email -->
+        <div class="col-lg-3">
+          <p><b>Contact</b></p>
+            <p>Email<br/><a href="samuel.valerie7@gmail.com" class="mail">samuel.valerie7@gmail.com</a></p>
+
+                    <!-- EDIT: address -->
+          <p><b>Adresse</b><br/>
+            2 Rue Carnot <br/>
+            Immeuble Vimeu, Appt 57<br/>
+            27000 &Eacute;vreux<br/>
+            France
+          </p>
+          <!-- EDIT: phone number -->
+          <p><b>Phone</b><br/>
+            <a href="tel:33 0644992739" class="tel">(+33)06 44 99 27 39</a> <br/>
+          </p>
+        </div>
+        <div class="col-lg-3 col-lg-offset-1">
+          <p><b>R&eacute;seaux Sociaux</b></p>
+          <p>
+            <!-- EDIT: Social Links. Replace # with your URL for each -->
+            <a href="https://www.linkedin.com/in/samuelvalerie/" target="_blank"><img src="/filrouge/linkedin.jpg" style="width:128px;height:128px;">
+            <!--<a href="#"><i class="icon-github"></i></a>-->
+          </p>
+        </div>
+      </div><!--/.row -->
+    </div><!--/.container -->
+  </div><!--/ #footer -->
+
+  </html>
+
+<?php
